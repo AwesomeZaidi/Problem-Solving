c3e8f4455cf29c096ff5ddbd60e1c959a52e302d
Asim Zaidi <asimzaidih@gmail.com>
2019-06-24 23:20:31 -0700

    building myReduce  and myPromise

--- JavaScript/Mock-Interviews/04.js
+++ JavaScript/Mock-Interviews/04.js
@@ -37,3 +37,78 @@ const matrix1 = [
 
 
 
+
+// console.log([1,2,3].reduce((c, v) => c + v));
+// Build your own myReduce function!
+
+Array.prototype.myReduce = function(fn, init) {
+    // this array [1,2,3]
+    // go over the  arr and every time, execute the fn
+    // provide, this.current index and the init val.
+    let current = init || this[0];
+    for (let i=0; i < this.length; i++) {
+        if (init == undefined) continue;
+        current = fn(this[i], current)
+    }
+    return current
+};
+
+// console.log([1,2,3].myReduce((c, v) => c + v,0));
+
+
+
+// PART 3
+
+
+
+// Implement your own Promise, myPromise.
+
+
+// My solution: UNFINISHED.
+
+let MyPromise = function(fn ) {
+    return (onSuccess, onError) => {
+
+    }
+}
+
+let myPromise = new MyPromise((res, rej) => {
+    // if () {
+    //     resolve(val);
+    // } else {
+    //     reject(err)
+    // }
+})
+
+// promise = new Promise(resolve, reject) => {
+
+// });
+
+
+
+// PART 4
+
+
+
+console.log(f(1)(2)(3)); // 9
+console.log(f(2)(2)(1)); // 4
+console.log(f(2,2,1)); // 4
+console.log(f()); // 0
+
+function f(a,b,c) {
+    if (a && b && c){
+        return (a + b)  * c
+    } else if (a  && b) {
+        return function(val) {
+            return (a + b) * val
+        }
+    } else if (a) {
+        return function(val1) {
+            return function(val2) {
+                return (a + val1) * val2;
+            }   
+        }
+    } else {
+        return 0;
+    }
+}
